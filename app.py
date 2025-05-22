c43d8062b9afca52cb6505ae3c5fbfbcc3a753f5
LittleKatyusha <abiyusuf2809@gmail.com>
2025-05-22 20:55:49 +0700

    asd

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def index():
 def get_perintah():
     return jsonify({"perintah": perintah_terakhir})
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
